ed41ef1ce4d4e8a211e712a53a43032668ae2595
fchung <fcchungg@gmail.com>
2022-04-26 16:20:32 -0700

    make it better

--- app.py
+++ app.py
@@ -1,72 +1,71 @@
 from flask import Flask, render_template, request
+from gad import highlightFace
 import cv2
-from keras.models import load_model
-import numpy as np
+import argparse
 import os
 
 app = Flask(__name__)
 
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
 
-
-@app.route('/')
+@app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route('/after', methods=['GET', 'POST'])
 def after():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image')
+
+    args = parser.parse_args()
+
+    faceProto = "opencv_face_detector.pbtxt"
+    faceModel = "opencv_face_detector_uint8.pb"
+    ageProto = "age_deploy.prototxt"
+    ageModel = "age_net.caffemodel"
+    genderProto = "gender_deploy.prototxt"
+    genderModel = "gender_net.caffemodel"
+
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+    ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    genderList = ['Male', 'Female']
+
+    faceNet = cv2.dnn.readNet(faceModel, faceProto)
+    ageNet = cv2.dnn.readNet(ageModel, ageProto)
+    genderNet = cv2.dnn.readNet(genderModel, genderProto)
+
+    padding = 20
     img = request.files['file1']
-    #print(img)
+    img.save(os.path.join(app.root_path, 'static/file.jpg'))
+    input_image = cv2.imread(os.path.join(app.root_path, 'static/file.jpg'))
 
-    img.save(os.path.join(app.root_path,'static/file.jpg'))
+    resultImg, faceBoxes = highlightFace(faceNet, input_image)
+    if not faceBoxes:
+        print("No face detected")
 
-    ####################################
-    img1 = cv2.imread(os.path.join(app.root_path,'static/file.jpg'))
-    #img1 = cv2.imread(img)
-    print(img1)
-    gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-    faces = cascade.detectMultiScale(gray, 1.1, 3)
+    for faceBox in faceBoxes:
+        face = input_image[max(0, faceBox[1] - padding):
+                           min(faceBox[3] + padding, input_image.shape[0] - 1), max(0, faceBox[0] - padding)
+                                                                                :min(faceBox[2] + padding,
+                                                                                     input_image.shape[1] - 1)]
 
-    for x,y,w,h in faces:
-        cv2.rectangle(img1, (x,y), (x+w, y+h), (0,255,0), 2)
+        blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        print(f'Gender: {gender}')
 
-        cropped = img1[y:y+h, x:x+w]
+        ageNet.setInput(blob)
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()]
+        print(f'Age: {age[1:-1]} years')
 
-    cv2.imwrite('static/after.jpg', img1)
+        cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8,
+                    (0, 255, 255), 2, cv2.LINE_AA)
 
-    try:
-        cv2.imwrite('static/cropped.jpg', cropped)
-
-    except:
-        pass
-
-    #####################################
-
-    try:
-        image = cv2.imread('static/cropped.jpg', 0)
-    except:
-        image = cv2.imread('static/file.jpg', 0)
-
-    image = cv2.resize(image, (48,48))
-
-    image = image/255.0
-
-    image = np.reshape(image, (1,48,48,1))
-
-    model = load_model('model_3.h5')
-
-    prediction = model.predict(image)
-
-    label_map = ['Anger','Neutral' , 'Fear', 'Happy', 'Sad', 'Surprise']
-
-    prediction = np.argmax(prediction)
-
-    final_prediction = label_map[prediction]
-
-    return render_template('after.html', data=final_prediction)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    cv2.imwrite('static/after.jpg', resultImg)
+    return render_template("after.html")
 
 
+
+if __name__ == '__main__':
+    app.run()
